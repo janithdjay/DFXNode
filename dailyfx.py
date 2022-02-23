b21b2aeaec0891ce5f39142ced538ec3b960e52a
Janith Jayawardana <85047102+janithdjay@users.noreply.github.com>
2022-02-23 15:59:11 +0530

    fixing req file

--- dailyfx.py
+++ dailyfx.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import camelot
+#import camelot
 import json
 from pathlib import Path
 import requests
@@ -9,11 +9,11 @@ import ghostscript
 
 pdfName = str(sys.argv[1])
 
-table = camelot.read_pdf("./public/" + pdfName, flavor='lattice') 
-table1 = table[0].df
-new_header = table1.iloc[0] #grab the first row for the header
-table1 = table1[1:] #take the data less the header row
-table1.columns = new_header
-table1.reset_index(drop=True)
-out = table1.to_json(orient='records')
-print(out)
+#table = camelot.read_pdf("./public/" + pdfName, flavor='lattice') 
+#table1 = table[0].df
+#new_header = table1.iloc[0] #grab the first row for the header
+#table1 = table1[1:] #take the data less the header row
+#table1.columns = new_header
+#table1.reset_index(drop=True)
+#out = table1.to_json(orient='records')
+print("works ok")
