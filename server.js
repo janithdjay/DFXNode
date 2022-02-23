1ff7ad71d13da88f42d6c49b2c5dfef7ee017f90
Janith Jayawardana <85047102+janithdjay@users.noreply.github.com>
2022-02-23 13:54:30 +0530

    updated py libs and port in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { spawn } = require('child_process')
 const jsonParser = bodyParser.json()
 
 const app = express()
-const PORT = 5500
+const PORT = process.env.PORT || 5000
 
 var pdfName = '';
 
@@ -112,6 +112,6 @@ app.use((req, res) => {
     res.status(404).render('404', { title: '404' })
 })
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
     console.log('Server running on port ' + PORT);
 })
